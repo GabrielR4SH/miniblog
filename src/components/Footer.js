1f62e5e3ca11349adcc5a5c6544716390a121d0b
GabrielR4SH <bieelsantana23@hotmail.com>
2023-10-21 22:16:02 -0300

    Navbar pronta

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Footer.module.css'
 
 function Footer() {
   return (
   <div>
-    <footer>
+    <footer className={styles.footer}>
             <h3>Escreva sobre oque você tem interesse!</h3>
             <p> MiniBlog &copy; 2023 </p>
         </footer>
